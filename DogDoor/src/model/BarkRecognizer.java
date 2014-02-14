db79389d1282f529a1dd75f5a42777f4354af260
drspy <caioamd@hotmail.com>
2014-02-14 13:42:27 -0200

    Adicionando classe que identifica latidos de um cachorro.

---
+++ DogDoor/src/model/BarkRecognizer.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: caioalmeida
+ * Date: 14/02/14
+ * Time: 13:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class BarkRecognizer {
+}
