1deda40c3b05384bfc6feb4191bc71d9b2181bad
drspy <caioamd@hotmail.com>
2014-02-14 13:49:13 -0200

    implementando classe que identifica latidos de um cachorro.

--- DogDoor/src/model/BarkRecognizer.java
+++ DogDoor/src/model/BarkRecognizer.java
@@ -1,11 +1,15 @@
 package model;
 
-/**
- * Created with IntelliJ IDEA.
- * User: caioalmeida
- * Date: 14/02/14
- * Time: 13:41
- * To change this template use File | Settings | File Templates.
- */
 public class BarkRecognizer {
+
+    protected DogDoor door;
+
+    public BarkRecognizer(DogDoor door) {
+        this.door = door;
+    }
+
+    public void recognizer(String bark) {
+        System.out.println("Detecting back: " + bark);
+        door.open();
+    }
 }
